3d5070c5e8c7394a1aa969c22a57630ec800d847
Alex Afonso <aa.santos95@gmail.com>
2020-10-14 16:05:19 -0300

    NLW Dia #3

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,69 +1,49 @@
-
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false,
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
 
-}
+const map = L.map("mapid", options).setView([-27.222633, -49.6455874], 16);
 
-
-
-const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 16)
-
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
-.addTo(map)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
 //create icon
 
 const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58,68],
-    iconAnchor:[29, 68],
-    popupAnchor:[170, 2]
+L.marker([-27.222633, -49.6455874], { icon: icon }).addTo(map);
 
-})
+function selectImage(event) {
+  const button = event.currentTarget;
 
+  //remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
 
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
 
-L.marker([-27.222633, -49.6455874],{icon: icon})
-.addTo(map)
+  //selecionar a imagem clicada
 
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
 
-function selectImage(event){
-    const button = event.currentTarget
-    
-    //remover todas as classes .active
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach(removeActiveClass)
+  //atualizar o container de imagem
 
-    function removeActiveClass(button){
+  imageContainer.src = image.src;
 
-        button.classList.remove("active")
-
-
-
-
-    }
-
-
-
-
-    //selecionar a imagem clicada
-
-    const image = button.children[0]
-    const imageContainer = document.querySelector(".orphanage-details > img")
-
-
-    //atualizar o container de imagem
-
-    imageContainer.src = image.src
-
-
-    // adicionar a classe .active para esse botão
-    button.classList.add("active")
+  // adicionar a classe .active para esse botão
+  button.classList.add("active");
 }
